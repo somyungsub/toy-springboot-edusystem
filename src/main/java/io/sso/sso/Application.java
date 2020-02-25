ff03456a51520dce33c0737f70e8f7146acd2ed7
Devso <gkdldy5@gmail.com>
2020-02-25 11:21:10 +0900

    Add : 교육전체리스트 Bean으로 일단 등록 -> 차후 캐시설정 등을 통해 관리

--- src/main/java/io/sso/sso/Application.java
+++ src/main/java/io/sso/sso/Application.java
@@ -1,13 +1,28 @@
 package io.sso.sso;
 
+import io.sso.sso.edu.Edu;
+import io.sso.sso.edu.EduRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
 
+  @Autowired
+  EduRepository eduRepository;
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  // TODO 캐시로 설정하여 관리, 일단 당분간은 Bean으로 활용
+  @Bean
+  public List<Edu> eduAllList() {
+    return eduRepository.findAll();
   }
 
 }
