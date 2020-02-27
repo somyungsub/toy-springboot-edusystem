343dad01519b1cce7f41be9b1f1c07a9dc2e8b76
Devso <gkdldy5@gmail.com>
2020-02-27 16:13:57 +0900

    Add : 차장 직급 관련 키워드 추가

--- src/main/java/io/sso/sso/classify/PositionTypeClassification.java
+++ src/main/java/io/sso/sso/classify/PositionTypeClassification.java
@@ -6,7 +6,7 @@ public enum PositionTypeClassification {
   ASSISTANT(new String[]{"기초","입문","베이직","기본","초급"}),
   ASSISTANT_MANAGER(new String[]{"기초","입문","베이직","기본"}),
   MANAGER(new String[]{"기초","입문","베이직","기본"}),
-  DUTY_MANAGER(new String[]{"기초","입문","베이직","기본"}),
+  DUTY_MANAGER(new String[]{"고급", "활용", "중급", "관리", "응용", "이용", "분석", "설계","아키","실전","실무"}),
   TEAM_READER(new String[]{"기초","입문","베이직","기본"}),
 
   ;
