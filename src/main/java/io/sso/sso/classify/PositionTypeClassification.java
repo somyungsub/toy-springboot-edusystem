e78c3bc750e6efffff8d9e5e6ae216606ed750eb
Devso <gkdldy5@gmail.com>
2020-02-27 17:40:48 +0900

    Add : 사원 직급 관련 키워드 추가

--- src/main/java/io/sso/sso/classify/PositionTypeClassification.java
+++ src/main/java/io/sso/sso/classify/PositionTypeClassification.java
@@ -3,7 +3,7 @@ package io.sso.sso.classify;
 
 public enum PositionTypeClassification {
 
-  ASSISTANT(new String[]{"기초","입문","베이직","기본","초급"}),
+  ASSISTANT(new String[]{"기초","입문","베이직","기본","초급","BASIC"}),
   ASSISTANT_MANAGER(new String[]{"기초","입문","베이직","기본"}),
   MANAGER(new String[]{"기초","입문","베이직","기본"}),
   DUTY_MANAGER(new String[]{"고급", "활용", "중급", "관리", "응용", "이용", "분석", "설계","아키","실전","실무"}),
